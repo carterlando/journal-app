4d35d8cdab999d76aa9597dc732bfcb938b2b931
Carter Lando <catalin.oanca@gmail.com>
2025-11-11 17:57:21 +0200

    Initial commit - Video Journal React app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'system-ui' }}>
+      <h1>Video Journal</h1>
+      <p>Building step by step on the server.</p>
+    </div>
+  )
+}
+
+export default App
